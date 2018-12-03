72e20d2d08f5b8ceceef1f9492fd33d37a5a0a34
Lucchetto <lucchetto.tie@live.com>
2018-12-03 15:57:39 +0100

    potter : Build CAF

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,6 +135,7 @@ BOARD_QTI_CAMERA_32BIT_ONLY := true
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_NO_CHARGER_LED := true
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.msm
 
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"

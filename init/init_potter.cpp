4679526e50ab81dde8d7c122043d8d5b3264ee8d
Nick van Bokhorst <nickvanbokhorst@hotmail.com>
2019-01-09 12:29:38 +0100

    potter: init: Add custom init with zram size for all variants
    *We have 2/3/4gb ram variants of potter, they all need a different amount of zram
    *Also add ro.boot.ram to detect right amount of ram, credits to @Vachounet

--- init/init_potter.cpp
+++ init/init_potter.cpp
@@ -31,6 +31,7 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 #include <android-base/logging.h>
+#include <sys/sysinfo.h>
 #include <android-base/properties.h>
 #include "property_service.h"
 
@@ -55,6 +56,20 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
+}
+
+/* Get Ram size for different variants */
+void check_device()
+{
+    struct sysinfo sys;
+    sysinfo(&sys);
+    if (sys.totalram > 3072ull * 1024 * 1024) {
+        property_set("ro.boot.ram", "4GB");
+    } else if (sys.totalram > 2048ull * 1024 * 1024) {
+        property_set("ro.boot.ram", "3GB");
+    } else {
+        property_set("ro.boot.ram", "2GB");
+    }
 }
 
 void num_sims() {
@@ -107,6 +122,8 @@ void vendor_load_properties()
     if (sku == "XT1683") {
         property_set("ro.hw.dtv", "true");
     }
+
+    check_device();
 }
 }  // namespace init
 } // namespace android

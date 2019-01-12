89385e03810a413fb04cb71d3c2395b7cdebaab3
kubersharma001 <20264908+kubersharma001@users.noreply.github.com>
2019-01-12 22:09:09 +0530

    potter: its time for Snap <3
    * [TEMP] potter: don't build FMRadio for now

--- device.mk
+++ device.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    Camera2 \
+    Snap \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
@@ -187,7 +187,7 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FMRadio \
+#    FMRadio \
     libfmjni \
     init.qti.fm.sh
 

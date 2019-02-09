82a373c1a8e51c0ba9c1a26cee51e4cc6913dcd6
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-02-09 15:12:38 +0000

    Potter : Disable SEPolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,7 +211,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 TARGET_RIL_VARIANT := caf
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Sensor

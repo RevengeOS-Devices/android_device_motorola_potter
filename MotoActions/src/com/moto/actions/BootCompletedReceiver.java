b5e2cd147b30cde56872e74a6191413ca08411ce
Abhishek Aggarwal <TheScarastic>
2019-02-17 14:40:03 +0530

    potter: dirac: Add DiracSound backend
    Special thanks to vasishath for his base work on mi3
    
    Signed-off-by: Ashwin R C <ashwin2001achu@gmail.com>
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- MotoActions/src/com/moto/actions/BootCompletedReceiver.java
+++ MotoActions/src/com/moto/actions/BootCompletedReceiver.java
@@ -28,6 +28,7 @@ import android.os.IBinder;
 import android.util.Log;
 
 import com.moto.actions.util.FileUtils;
+import com.moto.actions.dirac.DiracUtils;
 import com.moto.actions.actions.Constants;
 import com.moto.actions.ServiceWrapper.LocalBinder;
 
@@ -47,7 +48,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
 
         context.startService(new Intent(context, ServiceWrapper.class));
-    }
+        DiracUtils.initialize();
+   }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override

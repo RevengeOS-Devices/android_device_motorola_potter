5745c8603c44488a582f583531c9f390c9959630
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-03-25 05:19:46 +0000

    potter : enable SDCLANG
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,7 @@ TARGET_KERNEL_CONFIG := potter_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
 KERNEL_TOOLCHAIN := $(PWD)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
-# TARGET_USE_SDCLANG := true
+TARGET_USE_SDCLANG := true
 
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true

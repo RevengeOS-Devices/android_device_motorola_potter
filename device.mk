7859debb8629257946b9578c46ab0bf4d9be2c7b
UtsavisGreat <utsavbalar1231@gmail.com>
2019-07-06 15:59:12 +0000

    potter: build thermal controller
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>

--- device.mk
+++ device.mk
@@ -312,6 +312,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service
 
+#  Thermal Controller App
+PRODUCT_PACKAGES += \
+    ThermalController
+
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
